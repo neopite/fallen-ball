2775bee2bd88b5be07d44b8a578504b5fb7a3d14
Yaroslav <Stamin4ik4@gmail.com>
2021-04-04 20:26:16 +0300

    add new icons in settings and implment first version of jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _playerRG;
+    private PlayerMovement _playerMovement;
+
+    void Start()
+    {
+        _playerRG = GetComponent<Rigidbody2D>();
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _playerMovement.Move(new Vector3(GetClickSideNumber()/2 ,0.5f,0));
+        }
+    }
+
+    private float GetClickSideNumber()
+    {
+        return Screen.width / 2 > Input.mousePosition.x ? -1 : 1;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float _jumpForce;
+    private Rigidbody2D _playerRG;
+    private Vector3 _jumpDirection;
+    [SerializeField] [Range(-0.5f, 0.5f)] private float _angle;
+
+
+    void Start()
+    {
+        _playerRG = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Move(Vector3 direction)
+    {
+        direction.x = direction.x + _angle;
+        if (_playerRG.velocity.magnitude > 0.1f)
+        {
+            _playerRG.velocity = Vector2.zero;
+            
+        } 
+        _playerRG.AddForce(direction*_jumpForce,ForceMode2D.Impulse);
+    }
+}
