1d34f8178fd57c7994c841644dd6898890501c42
Yaroslav <Stamin4ik4@gmail.com>
2021-04-05 21:40:52 +0300

    add particle system , prefabs

---
+++ Assets/Scripts/BackgroundParticlesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BackgroundParticlesController : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _backgroundParticle;
+        private Transform _particlePoint;
+        private CameraController _cameraController;
+        [SerializeField] private float _stepRespawn;
+        private float _currentStep;
+
+        void Start()
+        {
+            _particlePoint = _backgroundParticle.GetComponent<Transform>();
+            _cameraController = GetComponent<CameraController>();
+            _backgroundParticle.Play();
+            _currentStep = _cameraController.MinY;
+        }
+
+        void Update()
+        {
+            if (_currentStep > _stepRespawn)
+            {
+                _particlePoint.position = new Vector3(0, _cameraController.MinY,12);
+                _currentStep = 0f;
+            }
+            else _currentStep += Math.Abs(_cameraController.MinY - _currentStep);
+            Debug.Log(_currentStep);
+        }
+
+        public void MoveTo(Vector3 target)
+        {
+            _particlePoint.position = target;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] private string _tagOfTarget;
+    [SerializeField] private float _cameraSpeed; 
+    
+    private Transform _mainPlayerPos;
+    private Transform _cameraTransform;
+
+    [SerializeField]private float _minY;
+
+    public float MinY
+    {
+        get { return _minY; }
+        private set { _minY = value; }
+    }
+
+    void Start()
+    {
+        if (_tagOfTarget == null)
+        {
+            _tagOfTarget = "Player";
+        }
+        _mainPlayerPos = GameObject.FindWithTag(_tagOfTarget).transform;
+        _cameraTransform = GetComponent<Transform>();
+        _cameraTransform.position = new Vector3(0,_mainPlayerPos.position.y , -10);
+        MinY = _cameraTransform.position.y - 5;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_mainPlayerPos)
+        {
+            if (_mainPlayerPos.position.y - MinY < 5)
+            {
+                Vector3 targetMove = new Vector3()
+                {
+                    x = 0,
+                    y = _cameraTransform.position.y,
+                    z = -10
+                };
+                Vector3 curPos = Vector3.Lerp(_cameraTransform.position, targetMove, _cameraSpeed);
+                _cameraTransform.position = curPos;
+            }
+            else
+            {
+                // if(_mainPlayerPos.position.y > _minY)
+                Vector3 targetMove = new Vector3()
+                {
+                    x = 0,
+                    y = _mainPlayerPos.position.y,
+                    z = -10
+                };
+                Vector3 curPos = Vector3.Lerp(_cameraTransform.position, targetMove, _cameraSpeed);
+                _cameraTransform.position = curPos;
+            }
+            if (MinY < _cameraTransform.position.y - 5) MinY = _cameraTransform.position.y - 5;
+        }
+    }
+}
