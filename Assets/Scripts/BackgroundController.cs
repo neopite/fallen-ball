c5e6bf9a95a41c7907aae62b9dde2a6f9d7c6486
Yaroslav <Stamin4ik4@gmail.com>
2021-04-11 11:58:35 +0300

    rewrite background structure

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackgroundController : MonoBehaviour
+{
+    private Transform _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GameObject.FindWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        gameObject.transform.position = new Vector3(_camera.position.x,_camera.position.y,20f);
+    }
+}
