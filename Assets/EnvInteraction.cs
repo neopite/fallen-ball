f2b58a1465c0aea8cf7cd177f90a4d1cd51db570
Yaroslav <Stamin4ik4@gmail.com>
2021-04-29 13:32:44 +0300

    add interaction with env

---
+++ Assets/EnvInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvInteraction : MonoBehaviour
+{
+    private Animator _animator;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) _animator.SetTrigger("isInteract");
+    }
+}
